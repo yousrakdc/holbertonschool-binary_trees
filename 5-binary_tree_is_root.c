0b4e19c9ac453555ae13b3753c59a4a8d8680b80
yousrakdc <yousrakdc@gmail.com>
2024-04-10 07:22:08 +0000

    mistake corrected

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,6 +4,7 @@
 
 /**
  * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to the node to check
  * Return: 1 if node is a root, otherwise 0
  * If node is NULL, return 0
  */
